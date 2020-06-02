300311c480ece93a81ae91b25434087e00c3df74
Christine Clemens <christineclemens.tesol@gmail.com>
2020-06-01 22:43:09 -0400

    Adjusted existing global variables.

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,12 @@
 let hourBlock = ["9 a.m.", "10 a.m.", "11 a.m.", "12 p.m.", "1 p.m.", "2 p.m.", "3 p.m.", "4 p.m.", "5 p.m."];
-let currentHour = "";
+let currentTime = moment().format('YYYY-MM-DD hh:mm a');
 
-function blockColor(time, theId) {
-    if (currentHour > time) {
+}
+
+function blockColor(time, inputId) {
+    if (currentTime > hourBlock) {
         document.querySelector("#inputId").addClass(".future");
-    } else if (currentHour == time) {
+    } else if (currentHour == hourBlock) {
         document.querySelector("#inputId").addClass(".present");
     } else {
         document.querySelector("#inputId").addClass(".past");
