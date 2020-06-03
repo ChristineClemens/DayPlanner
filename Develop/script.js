a9bde448c4f3b728dce2594d9d6c2b6142109bcd
Christine Clemens <christineclemens.tesol@gmail.com>
2020-06-02 20:27:35 -0400

    Added local time, added HTML elements, started save button functionality.

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,45 @@
-let hourBlock = ["9 a.m.", "10 a.m.", "11 a.m.", "12 p.m.", "1 p.m.", "2 p.m.", "3 p.m.", "4 p.m.", "5 p.m."];
-let currentTime = moment().format('YYYY-MM-DD hh:mm a');
+// let hourBlock = ["9 a.m.", "10 a.m.", "11 a.m.", "12 p.m.", "1 p.m.", "2 p.m.", "3 p.m.", "4 p.m.", "5 p.m."];
+// let currentTime = moment().format('YYYY-MM-DD hh:mm a');
 
+
+// function blockColor(time, inputId) {
+//     if (currentTime > hourBlock) {
+//         document.querySelector("#inputId").addClass(".future");
+//     } else if (currentHour == hourBlock) {
+//         document.querySelector("#inputId").addClass(".present");
+//     } else {
+//         document.querySelector("#inputId").addClass(".past");
+//     }
+// };
+
+//Add HTML timeblock elements.
+for (i = 9; i <= 17; i++) {
+    let retrieveTask = localStorage.getItem(i);
+    
+    document.querySelector(".container").innerHTML += 
+    `<div class='row'>
+        <textarea class='hour'>${moment(i.toString(),'LT').format('h A')}</textarea>
+        <input type='text' id='input${[i]}' class='description, time-block' placeholder='Enter tasks here...' value="${retrieveTask}">
+        <button type ='button' id='save${[i]}' class='btn, btn-primary, saveBtn' time=${[i]}><i class='far fa-save'></i>
+    </div>`;
+
+    if (retrieveTask == null) {
+        document.querySelector(`#input${[i]}`).value = "Enter tasks here..."
+    }
+}
+//Add local time.
+document.querySelector("#currentDay").innerHTML = `${moment().format('MMMM Do YYYY, h:mm:ss a')}`;
+
+//Save button functionality.
+function saveTask() {
+    let time = this.getAttribute("time");
+    console.log(time);
+    let inputTask = document.querySelector(`#input${time}`).value;
+    console.log(inputTask);
+    localStorage.setItem(time, inputTask);
 }
 
-function blockColor(time, inputId) {
-    if (currentTime > hourBlock) {
-        document.querySelector("#inputId").addClass(".future");
-    } else if (currentHour == hourBlock) {
-        document.querySelector("#inputId").addClass(".present");
-    } else {
-        document.querySelector("#inputId").addClass(".past");
-    }
-};
+for (i = 9; i <= 17; i++) {
+    var saveBtn = document.querySelector(`#save${i}`);
+    saveBtn.addEventListener("click", saveTask);
+}
