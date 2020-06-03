fabc06305f15ea532338cd4f314aaa77e169c2b6
Christine Clemens <christineclemens.tesol@gmail.com>
2020-06-02 22:05:14 -0400

    Finalize script.js file and add explanatory comments.

--- Develop/script.js
+++ Develop/script.js
@@ -1,41 +1,41 @@
-// let hourBlock = ["9 a.m.", "10 a.m.", "11 a.m.", "12 p.m.", "1 p.m.", "2 p.m.", "3 p.m.", "4 p.m.", "5 p.m."];
-// let currentTime = moment().format('YYYY-MM-DD hh:mm a');
 
-
-// function blockColor(time, inputId) {
-//     if (currentTime > hourBlock) {
-//         document.querySelector("#inputId").addClass(".future");
-//     } else if (currentHour == hourBlock) {
-//         document.querySelector("#inputId").addClass(".present");
-//     } else {
-//         document.querySelector("#inputId").addClass(".past");
-//     }
-// };
-
-//Add HTML timeblock elements.
+//Repeated rules for each time block.
 for (i = 9; i <= 17; i++) {
     let retrieveTask = localStorage.getItem(i);
-    
+    var someHTML;
+
+    //Retrieving tasks saved to local storage to display as persistent saved data.
+    if(retrieveTask == null){
+        someHTML = `placeholder='Enter task here...'`;
+    } else {
+        someHTML = `value='${retrieveTask}'`;
+    }
+
+    //Determining the colour of the textblock according to the current time.
+    if (moment().format('H') < i) {
+        blockColor = "future";
+    } else if (moment().format('H') == i) {
+        blockColor = "present";
+    } else {
+        blockColor = "past";
+    };
+
+    //Creating HTML textbox elements.
     document.querySelector(".container").innerHTML += 
     `<div class='row'>
         <textarea class='hour'>${moment(i.toString(),'LT').format('h A')}</textarea>
-        <input type='text' id='input${[i]}' class='description, time-block' placeholder='Enter tasks here...' value="${retrieveTask}">
+        <input type='text' id='input${[i]}' class='description, time-block, ${blockColor}' ${someHTML}>
         <button type ='button' id='save${[i]}' class='btn, btn-primary, saveBtn' time=${[i]}><i class='far fa-save'></i>
     </div>`;
-
-    if (retrieveTask == null) {
-        document.querySelector(`#input${[i]}`).value = "Enter tasks here..."
-    }
 }
-//Add local time.
+
+//Add local time to paragraph element.
 document.querySelector("#currentDay").innerHTML = `${moment().format('MMMM Do YYYY, h:mm:ss a')}`;
 
-//Save button functionality.
+//Save button functionality - save to local storage.
 function saveTask() {
     let time = this.getAttribute("time");
-    console.log(time);
     let inputTask = document.querySelector(`#input${time}`).value;
-    console.log(inputTask);
     localStorage.setItem(time, inputTask);
 }
 
